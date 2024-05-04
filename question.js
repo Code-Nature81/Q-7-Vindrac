a662b06357eef4b482210be89690dc3e363aa5ba
Code-Nature81 <81328065+Code-Nature81@users.noreply.github.com>
2024-05-04 17:01:50 +0200

    Add files via upload

---
+++ question.js
@@ -0,0 +1,29 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 7,
+    question: "A propos de temps, en personne avisé et après un petit « trabalh del cap » vous connaîtrez  l’heure approximative, grâce à ?",
+    answer: "2) un cadran solaire",
+    options: [
+      "1) une horloge sur le clocher",
+      "2) un cadran solaire",
+      "3) un habitant du village ",
+     
+    ]
+  },
+   
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
